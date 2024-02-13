293cc948c4f38bed9bbce66b75f4e4201cb48748
ImSamin <im.samin@gmail.com>
2024-02-13 21:52:05 +0600

    bug fixed

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 //import userService from './app/modules/user/user.service'
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import Routes from './app/routes';
-import httpStatus from 'http-status';
 
 const app: Application = express();
 
 // Cors and Parser
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/api/v1/', Routes);
 
 // Handle Not Found
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'Not Found',
